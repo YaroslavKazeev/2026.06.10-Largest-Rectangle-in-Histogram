cfe37a14d00dc6bca23e48e110551c7add3ac933
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-06-16 00:33:27 +0200

    feat: implement largest rectangle solution and add optimized monotonic stack alternative

---
+++ meta_ai_solution.py
@@ -0,0 +1,11 @@
+def largestRectangleArea(heights: list[int]) -> int:
+    stack = []
+    max_area = 0
+    for i in range(len(heights) + 1):
+        h = 0 if i == len(heights) else heights[i]
+        while stack and h < heights[stack[-1]]:
+            height = heights[stack.pop()]
+            width = i if not stack else i - stack[-1] - 1
+            max_area = max(max_area, height * width)
+        stack.append(i)
+    return max_area
--- my_solution.py
+++ my_solution.py
@@ -1,3 +1,37 @@
 def largestRectangleArea(heights: list[int]) -> int:
-    # Write your code here
-    pass
+    maxArea = 0
+    heightsDict = {}
+    residualIndexes = set()
+    for i, height in enumerate(heights):
+        residualIndexes.add(i)
+        if height in heightsDict:
+            heightsDict[height].append(i)
+        else:
+            heightsDict[height] = [i]
+
+    sortedHeights = sorted(heightsDict.keys())
+
+    firstHeight = sortedHeights[0]
+    maxArea = firstHeight * len(heights)
+    residualIndexes.difference_update(heightsDict[firstHeight])
+    sortedHeights.pop(0)
+
+    for height in sortedHeights:
+        indexes = heightsDict[height]
+        for i in indexes:
+            leftCurrInd = rightCurrInd = i
+            width = 1
+            while leftCurrInd - 1 in residualIndexes:
+                leftCurrInd -= 1
+                width += 1
+            while rightCurrInd + 1 in residualIndexes:
+                rightCurrInd += 1
+                width += 1
+
+            area = height * width
+            if area > maxArea:
+                maxArea = area
+
+            residualIndexes.remove(i)
+
+    return maxArea
